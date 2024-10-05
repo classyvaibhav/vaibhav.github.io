497f005d45d080cb78fbda6c41d39df66f8e75f1
vaibhav kawade <49197835+classyvaibhav@users.noreply.github.com>
2024-10-05 16:28:09 +0530

    Update info.js

--- info.js
+++ info.js
@@ -14,76 +14,40 @@ var commandsInfo = {
 
 /* Resume information*/
 
-var skillsInfo = "• C language\n" +
-  "• Python language\n" +
+var skillsInfo = "• Embedded C Programming\n" +
+  "• MATLAB\n" +
+  "• Circuit Design using Proteus and KiCad\n" +
+  "• PCB Design\n" +
+  "• Communication Protocols\n" +
   "• Git\n" +
   "• Linux\n" +
   "• Operating Systems concepts\n" +
   "• Basic networking concepts\n" +
   "• Basic web scraping knowledge\n" +
-  "• Basic web development (HTMl, CSS, JS, PHP)\n" +
-  "• Strong problem solving and debugging skills\n" +
+  "• Basic web development (HTML, CSS, JS, PHP)\n" +
+  "• Strong problem-solving and debugging skills\n" +
   "• Creativity and ability to learn quickly\n" +
   "• Exceptional communication\n" +
-  "• Successful working in a team environment, as well as independently \n" +
+  "• Successful working in a team environment, as well as independently\n" +
   "• The ability to work under pressure and multi-task\n" +
   "• The ability to follow instructions and deliver quality results\n" +
   "• The ability to quickly come up to speed on a new project\n";
 
+
 var experienceInfo = 
-"Dec 2018 - Jul 2020\n" +
-"RT-RK, Embedded Software Engineer\n" +
-"MotionWise project\n" +
-"The MotionWise project is a configurable Platform Software solution for Automotive ECUs (Electronic Control Unit) created by Austrian company TTTech\n" +
-"• Writing internal tools in Python for the RT-RK custom testing framework\n" +
-"• Fixing bugs in the RT-RK custom testing framework\n" +
-"• Writing new features in Python for the RT-RK custom testing framework\n" +
-"• Writing APIs in Python for Wireshark, CANoe and internal TTTech tools\n" +
-"• Reading C source code of the MotionWise core features in order to implement platform tests in Python\n" +
-"• Writing and automating tests for the Time Synchronization feature of MotionWise\n" +
-"• Writing documentation for tests in PTC software\n" +
-"• Writing documentation for APIs in Python\n" +
+"Dec 2022 - Present\n" +
+"President | SPARK CLUB, MIT Academy of Engineering\n" +
+"• Organized various tech workshops and events for students under Spark Club.\n" +
+"• Coordinated events to enhance technical skills and awareness in the college community.\n" +
 
+"\nApr 2024\n" +
+"Team Lead | TechnoPHILIA 2024\n" +
+"• Led a national-level project competition and poster presentation event.\n" +
+"• Managed a team to successfully execute the event, attracting participants from across the country.\n" +
 
-"\nApr 2016 - Dec 2018\n" +
-"RT-RK, Embedded Software Engineer\n" +
-"Harman project - Automotive Digital Television system\n" +
-"Software components and turnkey solutions for in-car STB (Set TopBox) and TV products based on iWedia software\n" +
-"• Developing STB features in C\n" +
-"• Debugging the software in GDB (multi-threaded C software)\n" +
-"• Creating a dynamic code analysis build using CLANG sanitizers\n" +
-
-
-"\nFeb 2016 - Apr 2016\n" + 
-"RT-RK, Embedded Software Engineering intern\n" + 
-"Harman project - Automotive Digital Television system\n" + 
-"Software components and turnkey solutions for in-car STB (Set TopBox) and TV products based on iWedia software\n" + 
-"• Debugging the software in GDB (multi-threaded C software)\n" + 
-"• Performing dynamic code analysis using Valgrind\n" + 
-
-
-"\nJun 2015 - Feb 2016\n" +
-"RT-RK, Compiler Tools Engineering intern\n" +
-"LLVM Project\n" +
-"The LLVM compiler infrastructure project is a collection of modular andreusable compiler and toolchain technologies used for developingcompiler frontends and backends\n" +
-"• Implementing and maintaining LLVM support for MIPS and microMIPS processors\n" +
-"• Translating the MIPS ISA (Instruction Set Architecture) to LLVM’s tableGen language\n" +
-"• Implementing MIPS instructions to the LLVM backend\n" +
-
-
-"\nSep 2013 - Feb 2014\n" +
-"T-COM,  Technical Support\n" +
-"Student job at Croatian Telecom T-COM\n" +
-"• Supporting telecom field technicians\n" +
-"• Remote configuration of DSLAM (Digital Subscriber Line AccessMultiplexer) devices\n" +
-"• Remote configuration of telecom user router\n" +
-
-
-"\nSep 2009 - Jun 2010\n" +
-"POINT, Computer Service Intern\n" +
-"Internship at a local computer service shop while in high school\n" +
-"• Installing Operating Systems on customer PC’s\n" +
-"• Diagnosing and fixing common PC hardware and software problems\n";
+"\nSep 2019 - Jun 2022\n" +
+"Cultural Head | Government Polytechnic\n" +
+"• Led the cultural committee in organizing and managing various events at the institute level.\n";
 
 
 var educationInfo = 
